71ed27aaf6306c8416c26633ee5ff78e22230be6
lucianapuentes <162376975+lucianapuentes@users.noreply.github.com>
2024-04-25 12:51:07 -0300

    Update Trie.py

--- practicas/tp-trie/code/Trie.py
+++ practicas/tp-trie/code/Trie.py
@@ -1,171 +1,127 @@
 class Trie:
-	root = None
-
+    root=None
 class TrieNode:
-    parent = None
-    children = None   
-    key = None
-    isEndOfWord = False
-class LinkedList:
-    head=None
-class Node:
-    value=None
-    nextNode=None
-def length(L):
-    elements=0
-    currentNode=L.head
-    while currentNode!=None:
-        elements=elements+1
-        currentNode=currentNode.nextNode
-    return elements
-def addend(L,element):
-    NewNode=Node()
-    NewNode.value=element
-    if L.head==None:
-        L.head=NewNode
-    else:
-        L.head.nextNode=NewNode
-def searchL(L,element):
-    currentNode=Node()
-    currentNode=L.head
-    position=0
-    while currentNode!=None:
-        if currentNode.value!=element:
-            return position
-        currentNode=currentNode.nextNode
-        position=position+1
-    if currentNode==None:
-        return None
-def accessL(L,position):
-    currentNode=Node()
-    currentNode=L.head
-    position_aux=0
-    element=None
-    if position==0:
-        element=L.head.value
-        return element
-    elif position>length(L):
-        return None
-    else:
-        while currentNode!=None:
-            if position_aux==position:
-                element=currentNode.value
-                return element
-            else:
-                position_aux=position_aux+1
-def Prevnode(L,pos):
-    i=0
-    current=L.head
-    while i<pos-1:
-        currentº=current.nextNode
-        i=i+1
-    return current
+    key=None
+    parent=None
+    children=None
+    isEndOfWord=False
 
-def deleteL(L,element):
-    currentNode=Node()
-    currentNode=L.head
-    position=searchL(L,element)
-    position_aux=0
-    if position==0:
-        L.head=L.head.nextNode
-        return
-    elif position==None
-        return
-    else:
-        while position-1>position_aux:
-            currentNode=currentNode.nextNode
-            position_aux=position_aux+1
-        
-        currentNode.nextNode=currentNode.nextNode.nextNode
-        return
-#insert(T,element) 
-#Descripción: insert un elemento en T, siendo T un Trie.
-#Entrada: El Trie sobre la cual se quiere agregar el elemento (Trie)  y el valor del elemento (palabra) a  agregar.
-#Salida:  No hay salida definida
-def insert(T,element):
-    if element==None:
-        return
-    else:
-        palabra=string()        
-        palabra=element
-        largo=palabra.len()
-        Node=Node()
-        Node.value=T.root.key
-        Node.nextNode=T.root.children.head
-        j=0
-        TNode=checkchildren(Node,palabra,j)
-        L=GenerateList(palabra,j,largo)
-        InsertR(T,L,TNode)
 
-def checkchildren(TNode,palabra,j):
-    if TNode.value==palabra[j]:
-        j=j+1 #cantidad de letras q ya estaban en el trie
-        if j==len(palabra):
-            return 
-        else:
-            if TNode.nextNode!=None:
-                TNode=TNode.nextNode
-                return checkchildren(TNode,palabra,j)
-            else:
-                return TNode #nodo padre
+def insert(T, element):
+    if T.root is None:
+        newNode = TrieNode()
+        newNode.key = None
+        T.root = newNode
+        T.root.parent = None
+    insert_R(T.root, element)
+
+
+def insert_R(current, element):
+
+    if current==None:
+        return
+    if len(element) < 1:
+        current.isEndOfWord = True
+        return
+
+    if current.children==None:
+        current.children = []
+        #current.children = LinkedList()
+        #current.children.head = None 
+
+    newChar = element[0]
+    childNode = None
+
+  # Check if a child node with the current character already exists
+    for child in current.children:
+        if child.key == newChar:
+            childNode = child
+            break
+
+  # If no child node exists with the current character, create a new one
+    if childNode is None:
+        newNode = TrieNode()
+        newNode.key = newChar
+        newNode.parent = current
+        current.children.append(newNode)
+        #add(current.children,newNode)
     else:
-        L=TNode.children
-        return prevnode(L,searchL(TNode(L,TNode.value)))
-def GenerateList(palabra,j,largo):
-    L=LinkedList()
-    for i in range(j,largo):
-        addend(L,palabra[i])
-   return L
-def InsertR(T,L,prevnode,j):
-    node=TrieNode()
-    node.key=L.head.value
-    if prevnode==None:
-        node.parent=None
-        T.root=node
+        newNode = childNode  #Assing childNode to newNode (for words containing other words)
+
+    insert_R(newNode, element[1:])
+
+def printTrie(node, level=0):
+
+    if node is None:
+        return
+
+  # Print the current node's key (or "root" if it's the root node)
+    if level == 0:  # Check if it's the root node
+        print("root")
     else:
-        node.parent=prevnode
-    delete(L,accessL(L,0))
-    node.children=L
-    if length(L)!=0:
-        prevnode=TrieNode()
-        prevnode=node
-        InsertR(T,L,prevnode)
-    else:
-        node.isEndOfWord=True
-        return 
+        print(" " * level + node.key +("*" if node.isEndOfWord else ""))  # Add * if it's the end of a word
+
+  # Recursively print children with proper indentation
+    if node.children:
+        for child_node in node.children:
+            printTrie(child_node,level + 4)  # Increase indentation level for children
+
+#printTrie(T.root)
+
 #search(T,element)
 #Descripción: Verifica que un elemento se encuentre dentro del Trie
 #Entrada: El Trie sobre la cual se quiere buscar el elemento (Trie)  y el valor del elemento (palabra)
 #Salida: Devuelve False o True  según se encuentre el elemento.
+
 def search(T,element):
-    if T==None or element==None:
-        return False
+    if element==None or T==None:
+        return 
     else:
-        palabra=string()        
-        palabra=element
-        Node=Node()
-        Node.value=T.root.key
-        Node.nextNode=T.root.children.head
-        return searchR(T.root.key,palabra,0)
-
-
-def searchR(TNode,palabra,j):
-    if TNode.value==palabra[j]:
-        j=j+1 #cantidad de letras q ya estaban en el trie
-        if j==len(palabra):
-            return True
-        else:
-            if TNode.nextNode!=None:
-                TNode=TNode.nextNode
-                return searchR(TNode,palabra,j)
+        return searchFirst(T.root,element)
+        
+def searchFirst(root,element):
+    if root.key!=element[0]:
+        if root.children!=None:
+            #print("hay children")
+            root=searchChild(root.children,element[0])
+            if root!=None:
+                #print("se encontro el primer caracter")
+                #print(root.key)
+                #print(root.children[0].key)
+                return searchR(root,element)
             else:
-     
-                   return False #nodo padre
-    else:
-        return False
-#delete(T,element)
-#Descripción: Elimina un elemento se encuentre dentro del Trie
-#Entrada: El Trie sobre la cual se quiere eliminar el elemento (Trie)  y el valor del elemento (palabra) a  eliminar.
-#Salida: Devuelve False o True  según se haya eliminado el elemento.
+                return False
+    elif root.key==element[0]:
+        return searchR(root,element)
 
+def searchChild(children,char):
+    for i in range(0,len(children)):
+        if children[i].key==char:
+            return children[i]
+    return None
+
+def searchR(Tnode,element):
+    #print("caracter a buscar",element[0])
+    #print(Tnode.key)
+    #print("fin de palabra",Tnode.isEndOfWord)
+    if Tnode.key==element[0]:
+            if len(element)==1:
+                if Tnode.isEndOfWord==True:
+                    #print("caso 2")
+                    #print("True")
+                    return True
+                else:
+                    #print("caso 3")
+                    #print("False")
+                    return False
+            else:
+                if Tnode.children!=None:
+                    child=searchChild(Tnode.children,element[1])
+                    if child!=None:
+                        return searchR(child,element[1:])
+                    else: 
+                        return False
+    else:
+        #print("caso 5")
+        return False
 
